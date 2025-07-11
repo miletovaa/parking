67c4fe3cd2e3c9af52af46e212a172761d4ae952
anna miletova <annamiletova@gmail.com>
2025-07-12 01:37:28 +0300

    Removed crud API test from the code

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,16 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
-import { use } from 'react'
-import { parkingApi } from '@/api'
-
 export default async function AdminDashboard() {
-
-    const parkings = use(parkingApi().list())
-
-    useEffect(() => {
-        console.log(parkings);
-    }, [parkings]);
     return (
         <>AdminDashboard</>
     )
